c39a724577cfabb04422ef425c31c394ccaa20c7
JCTRoth <jonasroth@mailbase.info>
2020-05-31 12:15:24 +0200

    restApi Cleanup

--- restApi.py
+++ restApi.py
@@ -6,7 +6,7 @@ import json
 from dbConnection import dbConnection
 
 # DB-Obj
-internal_dbConnection = dbConnection();  # Init. DB Connection
+internal_dbConnection = dbConnection()  # Init. DB Connection
 
 
 #
@@ -26,23 +26,24 @@ class DataBase(object):
             req_dict = json.loads(req_stream.decode('utf-8'))
 
         if req_dict["sql"] != None:  # If request is formated correctly
-            return_value = internal_dbConnection.run_sql(req_dict["sql"])  # Run SQL Command
+            return_value = internal_dbConnection.run_sql(
+                req_dict["sql"])  # Run SQL Command
             if return_value == "Error":
                 resp.body = '{ "message" : "SQL Error" }'
-                return #End Method
+                return  # End Method
             if return_value != None:
                 resp.body = json.dumps(return_value)
             else:
                 resp.body = '{ "message" : "No Return" }'
 
     def on_get(self, req, resp):
-        resp.body = json.dumps("Warnings: " + internal_dbConnection.returnWarnings())
+        resp.body = json.dumps(
+            "Warnings: " + internal_dbConnection.returnWarnings())
 
 
 #
 # Setup API
-#
-
+# Setup API
 api = falcon.API()
 
 # Define Endpoint Class-Obj
@@ -50,5 +51,5 @@ database_endpoint = DataBase()
 example_endpoint = Example()
 
 # Add them to API
-api.add_route('/testing', example_endpoint)
 api.add_route('/database', database_endpoint)
+api.add_route('/books')
